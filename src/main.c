7d4978d2661af80c37c431a0c3e9b500ab66fe82
Ilia Oshlakov <oshlakov2014@gmail.com>
2020-05-17 02:18:16 +0700

    Create a submenu with the "game" and "help" menu items"

--- src/main.c
+++ src/main.c
@@ -6,14 +6,70 @@
 
 int main(int argc, char *argv[])
 {
-    GtkWidget *window, *button, *button1, *vbox;
+    GtkWidget *window;
+    GtkWidget *button;
+    GtkWidget *button1;
+    GtkWidget *hbox;
+
+    GtkWidget *menubar;
+    GtkWidget *game_menu;
+    GtkWidget *settingMenu;
+    GtkWidget *sep;
+    GtkWidget *game_m;
+    GtkWidget *setting;
+    GtkWidget *num;
+    GtkWidget *word;
+    GtkWidget *quitMi;
+
+    GtkWidget *hbox1;
+    GtkWidget *help_m;
+    GtkWidget *help_menu;
+    GtkWidget *menubar1;
+    GtkWidget *rulesMi;
 
     gtk_init(&argc, &argv);
 
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Игра быки и коровы");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 200);
+    gtk_window_set_title(GTK_WINDOW(window), "Bulls and cows");
+    gtk_window_set_position(GTK_WINDOW(window), GTK_WIN_POS_CENTER);
+    gtk_window_set_default_size(GTK_WINDOW(window), 600, 300);
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
+
+    hbox = gtk_vbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(window), hbox);
+
+    game_menu = gtk_menu_new();
+    game_m = gtk_menu_item_new_with_label("Игра");
+
+    menubar = gtk_menu_bar_new();
+
+    settingMenu = gtk_menu_new();
+    setting = gtk_menu_item_new_with_label("Настройки");
+    num = gtk_menu_item_new_with_label("Игра с числами");
+    word = gtk_menu_item_new_with_label("Игра со словами");
+
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(setting), settingMenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(settingMenu), num);
+    gtk_menu_shell_append(GTK_MENU_SHELL(settingMenu), word);
+    sep = gtk_separator_menu_item_new();
+    quitMi = gtk_menu_item_new_with_label("Quit");
+
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(game_m), game_menu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(game_menu), setting);
+    gtk_menu_shell_append(GTK_MENU_SHELL(game_menu), sep);
+    gtk_menu_shell_append(GTK_MENU_SHELL(game_menu), quitMi);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), game_m);
+    gtk_box_pack_start(GTK_BOX(hbox), menubar, FALSE, FALSE, 0);
+
+    hbox1 = gtk_vbox_new(FALSE, 0);
+    gtk_container_add(GTK_CONTAINER(window), hbox1);
+
+    help_menu = gtk_menu_new();
+    help_m = gtk_menu_item_new_with_label("Помощь");
+    rulesMi = gtk_menu_item_new_with_label("Правила");
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(help_m), help_menu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(help_menu), rulesMi);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menubar), help_m);
 
     gtk_widget_show_all(window);
     gtk_main();
