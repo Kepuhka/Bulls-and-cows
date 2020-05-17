0324cb644a3e2e006fc966a43c552b5712fe714d
Ilia Oshlakov <oshlakov2014@gmail.com>
2020-05-17 21:34:21 +0700

    Add an input field, an input button, and a list of results

--- src/func.c
+++ src/func.c
@@ -1,7 +1,7 @@
 #include <gtk/gtk.h>
 #include <libintl.h>
 #include "func.h"
-#define _(x) gettext(x)
+#include <stdlib.h>
 
 void destroy(GtkWidget *widget, gpointer data)
 {
@@ -34,4 +34,38 @@ void word_activate(GtkMenuItem *menu_item, gpointer data)
     gtk_window_set_modal(GTK_WINDOW(window), TRUE);
 
     gtk_widget_show(window);
+}
+
+void append_item(GtkWidget *widget, gpointer entry)
+{
+
+    GtkListStore *store;
+    GtkTreeIter iter;
+
+    const gchar *str = gtk_entry_get_text(entry);
+
+    store = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(list)));
+
+    gtk_list_store_append(store, &iter);
+    gtk_list_store_set(store, &iter, LIST_ITEM, str, -1);
+
+    gtk_entry_set_text(entry, "");
+}
+
+void init_list(GtkWidget *list)
+{
+
+    GtkCellRenderer *renderer;
+    GtkTreeViewColumn *column;
+    GtkListStore *store;
+
+    renderer = gtk_cell_renderer_text_new();
+    column = gtk_tree_view_column_new_with_attributes("List Item", renderer, "text", LIST_ITEM, NULL);
+    gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+
+    store = gtk_list_store_new(N_COLUMNS, G_TYPE_STRING);
+
+    gtk_tree_view_set_model(GTK_TREE_VIEW(list), GTK_TREE_MODEL(store));
+
+    g_object_unref(store);
 }
--- src/func.h
+++ src/func.h
@@ -1,8 +1,19 @@
 #ifndef FUNC_H
 #define FUNC_H
 
+enum
+{
+
+    LIST_ITEM = 0,
+    N_COLUMNS
+};
+
+GtkWidget *list;
+
 void destroy(GtkWidget *widget, gpointer data);
 void number_activate(GtkMenuItem *menu_item, gpointer data);
 void word_activate(GtkMenuItem *menu_item, gpointer data);
+void append_item(GtkWidget *widget, gpointer entry);
+void init_list(GtkWidget *list);
 
 #endif
