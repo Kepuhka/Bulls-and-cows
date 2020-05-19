54f805266fe08c62b0b672bde2b137d3e56781f7
Kepuhka <kepuhka@localhost.localdomain>
2020-05-19 22:46:36 +0700

    Add a check of the number against the generated one and output the results

--- src/main.c
+++ src/main.c
@@ -1,5 +1,4 @@
 #include <gtk/gtk.h>
-#include <libintl.h>
 #include "func.h"
 
 int main(int argc, char *argv[])
@@ -90,7 +89,7 @@ int main(int argc, char *argv[])
     g_signal_connect(G_OBJECT(quitMi), "activate", G_CALLBACK(gtk_main_quit), NULL);
     g_signal_connect(G_OBJECT(num), "activate", G_CALLBACK(number_activate), NULL);
     g_signal_connect(G_OBJECT(word), "activate", G_CALLBACK(word_activate), NULL);
-    g_signal_connect(G_OBJECT(button_ok), "clicked", G_CALLBACK(append_item), entry);
+    g_signal_connect(G_OBJECT(button_ok), "clicked", G_CALLBACK(append_item_number), entry);
     g_signal_connect(G_OBJECT(window), "destroy", G_CALLBACK(destroy), NULL);
 
     gtk_widget_show_all(window);
